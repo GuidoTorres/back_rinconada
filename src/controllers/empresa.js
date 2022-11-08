18a895000f845941608d7c714b57439917cddba5
Hector <gt12930@GMAIL.com>
2022-11-08 09:44:09 -0500

    filter contrato empresa

--- src/controllers/empresa.js
+++ src/controllers/empresa.js
@@ -22,8 +22,9 @@ const getEmpresaById = async (req, res, next) => {
         include: [{ model: contrato, attributes: { exclude: ["contrato_id"] } }],
       }
     );
+    const filterContrato = all.map(item => item.contratos.length !== 0)
 
-    res.status(200).json({ data: all });
+    res.status(200).json({ data: filterContrato });
     next();
   } catch (error) {
     res.status(500).json();
