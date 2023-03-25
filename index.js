62799f296e1ca31330a055f8689d7f5f799b157f
Hector <gt12930@GMAIL.com>
2023-03-25 08:39:05 -0500

    correccion port

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const authRouter = require("./src/routes/auth");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 22;
 app.use(cors());
 app.use(express.json());
 
