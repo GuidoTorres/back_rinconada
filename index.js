f5c54c9dbb20ebf9637fd4abba85b72051180d79
Hector <gt12930@GMAIL.com>
2023-03-25 11:37:56 -0500

    correccion pouerto

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const authRouter = require("./src/routes/auth");
 
 const app = express();
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
