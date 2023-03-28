8f5d2d0baa8be8bde4926b91db86c1e55efec0da
Hector <gt12930@GMAIL.com>
2023-03-28 00:00:18 -0500

    correcciones

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ const { Sequelize, DataTypes } = require("sequelize");
 const DB_URI = process.env.DB_URI;
 
 const sequelize = new Sequelize({
-  database: "rinconada",
-  username: "root",
-  password: "root",
-  host: "localhost",
+  database: "heroku_30cfe8f0814e57f",
+  username: "bcbf9d2c2227ee",
+  password: "011e52da",
+  host: "us-cdbr-east-06.cleardb.net",
   dialect: "mysql",
-  port: "3306",
+  port: 3306,
   define: { timestamps: false, freezeTableName: true },
 });
 const dbConnect = async () => {
