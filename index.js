653eb6dc264fa5b65376bb0a8d492d3a326c3460
Hector <gt12930@GMAIL.com>
2023-03-30 11:58:18 -0500

    correcciones logistica finanzas

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const authRouter = require("./src/routes/auth");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
